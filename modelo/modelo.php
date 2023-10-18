238c203cadd4096be91a8453569dbe8f8eb3debf
Trevor-Velmont <montesgomezyoiner@gmail.com>
2023-10-18 07:08:44 -0500

    progreso

--- modelo/modelo.php
+++ modelo/modelo.php
@@ -1,3 +1,38 @@
 <?php
+// Conexión a la base de datos
+$conexion = new mysqli("localhost", "root", "", "usuarios");
 
+// Verifica si la conexión tiene errores
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Recopila los datos del formulario
+    $nombre = $_POST["nombres"];
+    $apellido = $_POST["apellidos"];
+    $telefono = $_POST["telefono"];
+    $correo = $_POST["correo"];
+    $contraseña = $_POST["contraseña"];
+
+    // Prepara la consulta SQL para insertar datos en la tabla "usuarios"
+    $sql = "INSERT INTO lista (Nombre, Apellido, Celular, Correo, Contraseña) VALUES (?, ?, ?, ?, ?)";
+
+    // Prepara la declaración
+    $stmt = $conexion->prepare($sql);
+    
+    // Vincula los parámetros
+    $stmt->bind_param("sssss", $nombre, $apellido, $telefono, $correo, $contraseña);
+    
+    // Ejecuta la consulta
+    if ($stmt->execute()) {
+        echo "Registro exitoso";
+    } else {
+        echo "Error al registrar: " . $stmt->error;
+    }
+    
+    // Cierra la declaración y la conexión
+    $stmt->close();
+    $conexion->close();
+}
 ?>
--- vista/JS/codigo.js
+++ vista/JS/codigo.js
@@ -2,8 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const botonRegistrate = document.getElementById("btn-regis");
     const formularioEmergente = document.getElementById("formulario-emergente");
     const cerrarFormulario = document.getElementById("cerrar-formulario");
-    const contrasenaRegis = document.getElementById("contraseña");
-    const confirmContrasena = document.getElementById("confirm-contraseña");
     const formularioRegistro = document.querySelector("#formulario-emergente form");
 
     // Muestra el formulario emergente al hacer clic en "Registrarse"
@@ -14,13 +12,5 @@ document.addEventListener("DOMContentLoaded", function() {
     // Oculta el formulario emergente al hacer clic en "Cerrar"
     cerrarFormulario.addEventListener("click", function() {
         formularioEmergente.style.display = "none";
-    });
-
-    // Validación al enviar el formulario
-    formularioRegistro.addEventListener("submit", function(event) {
-        if (contrasenaRegis.value !== confirmContrasena.value) {
-            alert("Las contraseñas no coinciden. Por favor, vuelva a intentar.");
-            event.preventDefault(); // Evita que el formulario se envíe
-        }
     });
 });
